b1f68135bbb24654f12409ed8804dc7de8c89420
Mr.programmer <58462135+Updeshprogrammer@users.noreply.github.com>
2026-01-05 18:22:29 +0530

    seo update

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,6 +15,29 @@ const robotoMono = Roboto_Mono({
 export const metadata = {
   title: "Fitness Tracker",
   description: "Track your diet and workout plans",
+  keywords: ["nextjs", "seo", "web development"],
+  authors: [{ name: "Updesh Kushwaha" }],
+  openGraph: {
+    title: "Fitness Tracker",
+    description: "Track your diet and workout plans",
+    url: "https://fitness-tracker-app-2026.vercel.app/",
+    siteName: "Fitness Tracker",
+    images: [
+      {
+        url: "https://fitness-tracker-app-2026.vercel.app/logo.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_IN",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Fitness Tracker",
+    description: "Track your diet and workout plans",
+    images: ["https://fitness-tracker-app-2026.vercel.app/logo.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
